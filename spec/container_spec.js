bb6470504ee9200ac2d5efb0060b37fac32e8a43
Chris Saylor <cjsaylor@gmail.com>
2013-08-10 22:25:20 -0400

    Updated auth header to new oauth specification.

--- spec/container_spec.js
+++ spec/container_spec.js
@@ -4,7 +4,7 @@ var util = require('../lib/util.js');
 
 var deleteContainer = function(container) {
 	frisby.create('Remove a container.')
-		.addHeader('Authentication', util.generateHeader(config.API_KEY, config.SECRET_KEY, '/containers/' + container.Container.slug))
+		.addHeader('Authentication', util.generateHeader(config.TOKEN))
 		.delete(config.HOST + '/containers/' + container.Container.slug)
 		.expectStatus(200)
 		.expectJSON({
@@ -13,62 +13,50 @@ var deleteContainer = function(container) {
 	.toss();
 };
 
+var fullContainer = {
+	Container: {
+		uuid: String,
+		name: String,
+		slug: String,
+		'container_item_count': Number,
+		created: String,
+		modified: String
+	},
+	Location: Object
+};
+
+var containerPreview = {
+	uuid: String,
+	slug: String
+}
+
 frisby.create('Test adding a container.')
-	.addHeader('Authentication', util.generateHeader(config.API_KEY, config.SECRET_KEY, '/containers', {name: 'Test Container 1'}))
+	.addHeader('Authentication', util.generateHeader(config.TOKEN))
 	.post(config.HOST + '/containers', {name: 'Test Container 1'})
 	.expectStatus(200)
-	.expectJSONTypes({
-		uuid: String,
-		slug: String
-	})
+	.expectJSONTypes(containerPreview)
 	.afterJSON(function(container) {
 		frisby.create('Get the created container.')
-			.addHeader('Authentication', util.generateHeader(config.API_KEY, config.SECRET_KEY, '/containers', {slug: container.slug}))
+			.addHeader('Authentication', util.generateHeader(config.TOKEN))
 			.get(config.HOST + '/containers?slug=' + container.slug)
 			.expectStatus(200)
-			.expectJSONTypes({
-				Container: function(val) {
-					expect(val).toContainJsonTypes({
-						uuid: String,
-						name: String,
-						slug: String,
-						container_item_count: Number,
-						created: String,
-						modified: String
-					})
-				},
-				Location: Object
-			})
+			.expectJSONTypes(fullContainer)
 			.afterJSON(deleteContainer)
 		.toss();
 	})
 .toss();
 
 frisby.create('Test adding and editing a container.')
-	.addHeader('Authentication', util.generateHeader(config.API_KEY, config.SECRET_KEY, '/containers', {name: 'Test Container 2'}))
+	.addHeader('Authentication', util.generateHeader(config.TOKEN))
 	.post(config.HOST + '/containers', {name: 'Test Container 2'})
 	.expectStatus(200)
-	.expectJSONTypes({
-		uuid: String,
-		slug: String
-	})
+	.expectJSONTypes(containerPreview)
 	.afterJSON(function(container) {
 		frisby.create('Edit the created container.')
-			.addHeader('Authentication', util.generateHeader(config.API_KEY, config.SECRET_KEY, '/containers/' + container.slug, {name: 'Test Container 2 alt'}))
+			.addHeader('Authentication', util.generateHeader(config.TOKEN))
 			.put(config.HOST + '/containers/' + container.slug, {name: 'Test Container 2 alt'})
 			.expectStatus(200)
-			.expectJSONTypes({
-				Container: function(val) {
-					expect(val).toContainJsonTypes({
-						uuid: String,
-						name: String,
-						slug: String,
-						container_item_count: Number,
-						created: String,
-						modified: String
-					})
-				}
-			})
+			.expectJSONTypes(fullContainer)
 			.afterJSON(deleteContainer)
 		.toss();
 	})
--- spec/user_spec.js
+++ spec/user_spec.js
@@ -2,22 +2,18 @@ var frisby = require('frisby');
 var config = require('../config');
 
 frisby.create('Test a successful user login')
-	.post(config.HOST + '/users/login', {email: config.EMAIL, password: config.PASSWORD})
+	.post(config.HOST + '/users/login', {email: config.EMAIL, password: config.PASSWORD, application: 'Boxmeup API Tester'})
 	.expectStatus(200)
 	.expectJSONTypes({
-		id: String,
-		email: String,
-		uuid: String,
-		'api_key': String,
-		'secret_key': String
+		token: String
 	})
-.toss()
+.toss();
 
 frisby.create('Test a failed user login.')
-	.post(config.HOST + '/users/login', {email: 'non-existant-email', password: 'rubish'})
+	.post(config.HOST + '/users/login', {email: 'non-existant-email', password: 'rubish', application: 'Boxmeup API Tester'})
 	.expectStatus(403)
 	.expectJSON({
 		name: "Forbidden",
 		url: '/api/users/login'
 	})
-.toss()
+.toss();
